2bddc423f1c9ce0101f0dd40effca33e6c13db1f
Iivari Äikäs <iivari.aikas@gmail.com>
2020-01-16 16:48:54 +0200

    Implement simple timestamp print loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use chrono::prelude::*;
+use chrono::Duration;
+use std::thread::sleep;
+
 fn main() {
     println!("Hello, world!");
+    loop {
+        let now = Utc::now();
+        let output = now.format("%a %Y-%m-%d - %H:%M:%S").to_string();
+        println!("{}", output);
+        let sleep_duration = Duration::seconds(1)
+            .to_std()
+            .expect("sleep duration generation failed");
+        sleep(sleep_duration);
+    }
 }
