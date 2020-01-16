7f899d438a8a3ffe7649b07431425d0b0f8cee53
Iivari Äikäs <iivari.aikas@gmail.com>
2020-01-16 20:30:41 +0200

    Draft out generic trait for generators

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
-use chrono::prelude::*;
-use chrono::Duration;
+mod timestamp;
+
 use std::sync::mpsc::{channel, Sender};
 use std::thread;
 use structopt::StructOpt;
 
-#[derive(Debug)]
-enum TimeAccuracy {
-    Seconds,
-    Minutes,
-}
-
 #[derive(Debug, StructOpt)]
 #[structopt(about)]
 struct Opt {
+    /// Set timestamp update accuracy: seconds, minutes
     #[structopt(long, parse(from_str = parse_accuracy), default_value = "seconds")]
-    /// set timestamp update accuracy: seconds, minutes
-    time_accuracy: TimeAccuracy,
+    time_accuracy: timestamp::Accuracy,
+
+    /// Enable debug printing to stderr
     #[structopt(long, short)]
-    debug: bool
+    debug: bool,
 }
 
-fn parse_accuracy(arg: &str) -> TimeAccuracy {
+fn parse_accuracy(arg: &str) -> timestamp::Accuracy {
     let lowered = arg.to_lowercase();
     if lowered == "minutes" {
-        TimeAccuracy::Minutes
+        timestamp::Accuracy::Minutes
     } else if lowered == "seconds" {
-        TimeAccuracy::Seconds
+        timestamp::Accuracy::Seconds
     } else {
-        eprintln!(
-            "Invalid time_accuracy {:?}, defaulting to seconds",
-            &lowered
-        );
-        TimeAccuracy::Seconds
+        eprintln!("Invalid accuracy {:?}, defaulting to seconds", &lowered);
+        timestamp::Accuracy::Seconds
     }
+}
+
+pub type GeneratorResult<T> = Result<(), T>;
+
+pub trait OutputGenerator {
+    type Config;
+    type Error;
+
+    fn new(tx: Sender<String>, config: Self::Config) -> Self;
+    fn generate(&self) -> GeneratorResult<Self::Error>;
 }
 
 fn main() {
@@ -44,36 +47,18 @@ fn main() {
     let (tx, rx) = channel();
 
     let timestamp_tx = tx.clone();
-    thread::spawn(move || generate_timestamps(timestamp_tx, opt.time_accuracy));
+    let timestamp_config = timestamp::TimestampConfig {
+        debug: opt.debug,
+        accuracy: opt.time_accuracy,
+    };
+
+    thread::spawn(move || {
+        let t = timestamp::TimestampGenerator::new(timestamp_tx, timestamp_config);
+        t.generate().unwrap();
+    });
 
     loop {
         let timestamp = rx.recv().unwrap();
         println!("{}", timestamp);
     }
-}
-
-fn generate_timestamps(tx: Sender<String>, accuracy: TimeAccuracy) {
-    loop {
-        let now = Utc::now();
-        let output = now.format("%a %Y-%m-%d - %H:%M:%S").to_string();
-        tx.send(output).unwrap();
-
-        match calculate_sleep_duration(&accuracy).to_std() {
-            Ok(sleep_duration) => thread::sleep(sleep_duration),
-            // Conversion to std::time::Duration fails if chrono::Duration is negative, which can
-            // happen if time has been adjusted to past. Just try again until system time has
-            // been stabilized.
-            Err(_) => continue,
-        }
-    }
-}
-
-fn calculate_sleep_duration(accuracy: &TimeAccuracy) -> Duration {
-    let now = Utc::now();
-    let wakeup_point = match accuracy {
-        // hardcoded `with_second` parameter inside valid 0-59 range, will never return None
-        TimeAccuracy::Minutes => (now + Duration::minutes(1)).with_second(0).unwrap(),
-        TimeAccuracy::Seconds => now + Duration::seconds(1),
-    };
-    wakeup_point.signed_duration_since(now)
 }
---
+++ src/timestamp.rs
@@ -0,0 +1,63 @@
+use crate::{GeneratorResult, OutputGenerator};
+
+use chrono::prelude::*;
+use chrono::Duration;
+use std::sync::mpsc::Sender;
+use std::thread;
+
+#[derive(Debug)]
+pub enum Accuracy {
+    Seconds,
+    Minutes,
+}
+
+#[derive(Debug)]
+pub struct TimestampConfig {
+    pub debug: bool,
+    pub accuracy: Accuracy,
+}
+
+#[derive(Debug)]
+pub struct TimestampGenerator {
+    config: TimestampConfig,
+    tx: Sender<String>,
+}
+
+impl OutputGenerator for TimestampGenerator {
+    type Config = TimestampConfig;
+    type Error = ();
+
+    fn new(tx: Sender<String>, config: TimestampConfig) -> Self {
+        TimestampGenerator { config, tx }
+    }
+
+    fn generate(&self) -> GeneratorResult<Self::Error> {
+        loop {
+            let now = Utc::now();
+            let output = now.format("%a %Y-%m-%d - %H:%M:%S").to_string();
+
+            match self.tx.send(output) {
+                Err(_) => return Err(()),
+                Ok(_) => {}
+            };
+
+            match calculate_sleep_duration(&self.config.accuracy).to_std() {
+                Ok(sleep_duration) => thread::sleep(sleep_duration),
+                // Conversion to std::time::Duration fails if chrono::Duration is negative, which can
+                // happen if time has been adjusted to past. Just try again until system time has
+                // been stabilized.
+                Err(_) => continue,
+            }
+        }
+    }
+}
+
+fn calculate_sleep_duration(accuracy: &Accuracy) -> Duration {
+    let now = Utc::now();
+    let wakeup_point = match accuracy {
+        // hardcoded `with_second` parameter inside valid 0-59 range, will never return None
+        Accuracy::Minutes => (now + Duration::minutes(1)).with_second(0).unwrap(),
+        Accuracy::Seconds => now + Duration::seconds(1),
+    };
+    wakeup_point.signed_duration_since(now)
+}
