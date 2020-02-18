a20ee901363007af5d421ab02a5fb1db1bf9e23e
Iivari Äikäs <iivari.aikas@gmail.com>
2020-02-18 14:49:03 +0200

    Properly report that battery state did change

--- src/output.rs
+++ src/output.rs
@@ -48,20 +48,24 @@ impl Output {
             }
             Output::Battery(ref mut bat) => match update {
                 UpdateType::Percentage(p) => {
+                    let changed = bat.percentage != p;
                     bat.percentage = p;
-                    bat.percentage == p
+                    changed
                 }
                 UpdateType::OnBattery(b) => {
+                    let changed = bat.on_battery != b;
                     bat.on_battery = b;
-                    bat.on_battery == b
+                    changed
                 }
                 UpdateType::TimeToFull(t) => {
+                    let changed = bat.seconds_to_full != t;
                     bat.seconds_to_full = t;
-                    bat.seconds_to_full == t
+                    changed
                 }
                 UpdateType::TimeToEmpty(t) => {
+                    let changed = bat.seconds_to_empty != t;
                     bat.seconds_to_empty = t;
-                    bat.seconds_to_empty == t
+                    changed
                 }
                 _ => false,
             },
@@ -130,6 +134,18 @@ mod tests {
     }
 
     #[test]
+    fn matching_noop_timestamp_update() {
+        let ts = timestamp_output();
+        let mut out = Output::Timestamp(ts);
+        let update = UpdateType::Timestamp("".to_string());
+        let changed = out.update(update);
+        assert!(!changed);
+
+        let expected = Output::Timestamp(timestamp_output());
+        assert_eq!(out, expected);
+    }
+
+    #[test]
     fn nonmatching_timestamp_updates() {
         let ts = timestamp_output();
         let mut out = Output::Timestamp(ts);
@@ -156,6 +172,81 @@ mod tests {
         assert_eq!(out, unchanged_out);
     }
 
+    #[test]
+    fn matching_battery_updates() {
+        let mut out = Output::Battery(battery_output());
+        let update = UpdateType::OnBattery(false);
+        assert!(out.update(update));
+        let expected = Output::Battery(Battery {
+            on_battery: false,
+            ..battery_output()
+        });
+        assert_eq!(out, expected);
+
+        let mut out = Output::Battery(battery_output());
+        let update = UpdateType::Percentage(66.6);
+        assert!(out.update(update));
+        let expected = Output::Battery(Battery {
+            percentage: 66.6,
+            ..battery_output()
+        });
+        assert_eq!(out, expected);
+
+        let mut out = Output::Battery(battery_output());
+        let update = UpdateType::TimeToFull(12);
+        assert!(out.update(update));
+        let expected = Output::Battery(Battery {
+            seconds_to_full: 12,
+            ..battery_output()
+        });
+        assert_eq!(out, expected);
+
+        let mut out = Output::Battery(battery_output());
+        let update = UpdateType::TimeToEmpty(12);
+        assert!(out.update(update));
+        let expected = Output::Battery(Battery {
+            seconds_to_empty: 12,
+            ..battery_output()
+        });
+        assert_eq!(out, expected);
+    }
+
+    #[test]
+    fn matching_noop_battery_updates() {
+        let mut out = Output::Battery(battery_output());
+        let update = UpdateType::OnBattery(true);
+        assert!(!out.update(update));
+        let expected = Output::Battery(battery_output());
+        assert_eq!(out, expected);
+
+        let mut out = Output::Battery(battery_output());
+        let update = UpdateType::Percentage(41.5);
+        assert!(!out.update(update));
+        let expected = Output::Battery(battery_output());
+        assert_eq!(out, expected);
+
+        let mut out = Output::Battery(battery_output());
+        let update = UpdateType::TimeToFull(0);
+        assert!(!out.update(update));
+        let expected = Output::Battery(battery_output());
+        assert_eq!(out, expected);
+
+        let mut out = Output::Battery(battery_output());
+        let update = UpdateType::TimeToEmpty(36123);
+        assert!(!out.update(update));
+        let expected = Output::Battery(battery_output());
+        assert_eq!(out, expected);
+    }
+
+    #[test]
+    fn nonmatching_battery_updates() {
+        let mut out = Output::Battery(battery_output());
+        let update = UpdateType::Timestamp("wow, string".to_string());
+        assert!(!out.update(update));
+        let expected = Output::Battery(battery_output());
+        assert_eq!(out, expected);
+    }
+
     fn timestamp_output() -> Timestamp {
         Timestamp {
             state: "".to_string(),
@@ -163,6 +254,15 @@ mod tests {
                 accuracy: Accuracy::Minutes,
                 format: "%a %Y-%m-%d - %H:%M:%S".to_string(),
             },
+        }
+    }
+
+    fn battery_output() -> Battery {
+        Battery {
+            percentage: 41.5,
+            on_battery: true,
+            seconds_to_full: 0,
+            seconds_to_empty: 36123,
         }
     }
 }
