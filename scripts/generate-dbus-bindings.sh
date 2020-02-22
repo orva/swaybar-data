5613dbca0cb0652d1eddaf48af6d065e84fa0821
Iivari Äikäs <iivari.aikas@gmail.com>
2020-02-22 15:03:11 +0200

    Parametrize binding generation

--- scripts/generate-dbus-bindings.sh
+++ scripts/generate-dbus-bindings.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 #
-# NOTE: to run this script, it needs some editing!
-#
 # You need to provide IDs to required DBus objects, use busctl, dbus-send, or
-# dbus client of your choice to find ones with required interface
+# dbus client of your choice to find ones with required interface(s)
 
 set -o errexit
 set -o nounset
@@ -13,63 +11,85 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 pushd "$SCRIPTPATH" > /dev/null || exit
 trap "popd > /dev/null" EXIT
 
-WIRELESS_DEVICE_ID=2
-WIRED_DEVICE_ID=3
-ACCESS_POINT_ID=302
+UPOWER=${UPOWER:-0}
+NETWORK_MANAGER=${NETWORK_MANAGER:-0}
+ACTIVE_CONNECTION_ID=${ACTIVE_CONNECTION_ID:-0}
+WIRELESS_DEVICE_ID=${WIRELESS_DEVICE_ID:-0}
+WIRED_DEVICE_ID=${WIRED_DEVICE_ID:-0}
+ACCESS_POINT_ID=${ACCESS_POINT_ID:-0}
 
-echo "## Generating DBus.Properties (from UPower)"
-dbus-codegen-rust --system-bus --generic-variant --methodtype None  \
-    --destination org.freedesktop.UPower                            \
-    --skipprefix org.freedesktop                                    \
-    --interfaces org.freedesktop.DBus.Properties                    \
-    --path /org/freedesktop/UPower > ../src/generated/dbus_properties.rs
+if [[ "$UPOWER" != "0" ]]; then
+    echo "## Generating DBus.Properties (from UPower)"
+    dbus-codegen-rust --system-bus --generic-variant --methodtype None  \
+        --destination org.freedesktop.UPower                            \
+        --skipprefix org.freedesktop                                    \
+        --interfaces org.freedesktop.DBus.Properties                    \
+        --path /org/freedesktop/UPower > ../src/generated/dbus_properties.rs
 
-echo "## Generating UPower"
-dbus-codegen-rust --system-bus --generic-variant --methodtype None  \
-    --destination org.freedesktop.UPower                            \
-    --skipprefix org.freedesktop                                    \
-    --interfaces org.freedesktop.UPower                             \
-    --path /org/freedesktop/UPower > ../src/generated/upower.rs
+    echo "## Generating UPower"
+    dbus-codegen-rust --system-bus --generic-variant --methodtype None  \
+        --destination org.freedesktop.UPower                            \
+        --skipprefix org.freedesktop                                    \
+        --interfaces org.freedesktop.UPower                             \
+        --path /org/freedesktop/UPower > ../src/generated/upower.rs
 
-echo "## Generating UPower.Device"
-dbus-codegen-rust --system-bus --generic-variant --methodtype None  \
-    --destination org.freedesktop.UPower                            \
-    --skipprefix org.freedesktop                                    \
-    --interfaces org.freedesktop.UPower.Device                      \
-    --path /org/freedesktop/UPower/devices/DisplayDevice > ../src/generated/upower_device.rs
+    echo "## Generating UPower.Device"
+    dbus-codegen-rust --system-bus --generic-variant --methodtype None  \
+        --destination org.freedesktop.UPower                            \
+        --skipprefix org.freedesktop                                    \
+        --interfaces org.freedesktop.UPower.Device                      \
+        --path /org/freedesktop/UPower/devices/DisplayDevice > ../src/generated/upower_device.rs
+fi
 
-echo "## Generating NetworkManager"
-dbus-codegen-rust --system-bus --generic-variant --methodtype None  \
-    --destination org.freedesktop.NetworkManager                    \
-    --skipprefix org.freedesktop                                    \
-    --interfaces org.freedesktop.NetworkManager                     \
-    --path /org/freedesktop/NetworkManager > ../src/generated/network_manager.rs
+if [[ "$NETWORK_MANAGER" != "0" ]]; then
+    echo "## Generating NetworkManager"
+    dbus-codegen-rust --system-bus --generic-variant --methodtype None  \
+        --destination org.freedesktop.NetworkManager                    \
+        --skipprefix org.freedesktop                                    \
+        --interfaces org.freedesktop.NetworkManager                     \
+        --path /org/freedesktop/NetworkManager > ../src/generated/network_manager.rs
+fi
 
-echo "## Generating NetworkManager.Device"
-dbus-codegen-rust --system-bus --generic-variant --methodtype None      \
-    --destination org.freedesktop.NetworkManager                        \
-    --skipprefix org.freedesktop                                        \
-    --interfaces org.freedesktop.NetworkManager.Device                  \
-    --path /org/freedesktop/NetworkManager/Devices/${WIRELESS_DEVICE_ID} > ../src/generated/nm_device.rs
+if [[ "$ACTIVE_CONNECTION_ID" != "0" ]]; then
+    echo "## Generating NetworkManager.Connection.Active"
+    dbus-codegen-rust --system-bus --generic-variant --methodtype None      \
+        --destination org.freedesktop.NetworkManager                        \
+        --skipprefix org.freedesktop                                        \
+        --interfaces org.freedesktop.NetworkManager.Connection.Active       \
+        --path "/org/freedesktop/NetworkManager/ActiveConnection/${ACTIVE_CONNECTION_ID}" > ../src/generated/nm_active_connection.rs
+fi
 
-echo "## Generating NetworkManager.Device.Wireless"
-dbus-codegen-rust --system-bus --generic-variant --methodtype None      \
-    --destination org.freedesktop.NetworkManager                        \
-    --skipprefix org.freedesktop                                        \
-    --interfaces org.freedesktop.NetworkManager.Device.Wireless         \
-    --path /org/freedesktop/NetworkManager/Devices/${WIRELESS_DEVICE_ID} > ../src/generated/nm_device_wireless.rs
+if [[ "$WIRELESS_DEVICE_ID" != "0" ]]; then
+    echo "## Generating NetworkManager.Device"
+    dbus-codegen-rust --system-bus --generic-variant --methodtype None      \
+        --destination org.freedesktop.NetworkManager                        \
+        --skipprefix org.freedesktop                                        \
+        --interfaces org.freedesktop.NetworkManager.Device                  \
+        --path "/org/freedesktop/NetworkManager/Devices/${WIRELESS_DEVICE_ID}" > ../src/generated/nm_device.rs
 
-echo "## Generating NetworkManager.Device.Wired"
-dbus-codegen-rust --system-bus --generic-variant --methodtype None      \
-    --destination org.freedesktop.NetworkManager                        \
-    --skipprefix org.freedesktop                                        \
-    --interfaces org.freedesktop.NetworkManager.Device.Wired            \
-    --path /org/freedesktop/NetworkManager/Devices/${WIRED_DEVICE_ID} > ../src/generated/nm_device_wired.rs
+    echo "## Generating NetworkManager.Device.Wireless"
+    dbus-codegen-rust --system-bus --generic-variant --methodtype None      \
+        --destination org.freedesktop.NetworkManager                        \
+        --skipprefix org.freedesktop                                        \
+        --interfaces org.freedesktop.NetworkManager.Device.Wireless         \
+        --path "/org/freedesktop/NetworkManager/Devices/${WIRELESS_DEVICE_ID}" > ../src/generated/nm_device_wireless.rs
+fi
 
-echo "## Generating NetworkManager.AccessPoint"
-dbus-codegen-rust --system-bus --generic-variant --methodtype None      \
-    --destination org.freedesktop.NetworkManager                        \
-    --skipprefix org.freedesktop                                        \
-    --interfaces org.freedesktop.NetworkManager.AccessPoint             \
-    --path /org/freedesktop/NetworkManager/AccessPoint/${ACCESS_POINT_ID} > ../src/generated/nm_access_point.rs
+if [[ "$WIRED_DEVICE_ID" != "0" ]]; then
+    echo "## Generating NetworkManager.Device.Wired"
+    dbus-codegen-rust --system-bus --generic-variant --methodtype None      \
+        --destination org.freedesktop.NetworkManager                        \
+        --skipprefix org.freedesktop                                        \
+        --interfaces org.freedesktop.NetworkManager.Device.Wired            \
+        --path "/org/freedesktop/NetworkManager/Devices/${WIRED_DEVICE_ID}" > ../src/generated/nm_device_wired.rs
+fi
+
+if [[ "$ACCESS_POINT_ID" != "0" ]]; then
+    echo "## Generating NetworkManager.AccessPoint"
+    dbus-codegen-rust --system-bus --generic-variant --methodtype None      \
+        --destination org.freedesktop.NetworkManager                        \
+        --skipprefix org.freedesktop                                        \
+        --interfaces org.freedesktop.NetworkManager.AccessPoint             \
+        --path "/org/freedesktop/NetworkManager/AccessPoint/${ACCESS_POINT_ID}" > ../src/generated/nm_access_point.rs
+fi
 
